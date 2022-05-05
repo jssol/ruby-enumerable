f56eb083541d2c22970244a70226821d01961d52
Bavon101 <bavonfate@gmail.com>
2022-05-05 23:32:11 +0300

    Create Mylist class and include MyEnumarable

---
+++ my_list.rb
@@ -0,0 +1,20 @@
+require './my_enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+end
+
+# test the provided test-cases
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
+
+puts(list.filter(&:even?))
